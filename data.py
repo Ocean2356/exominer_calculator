46231885a459e8a48f6d451b49499d2498af4b1e
Ocean2356 <haiyang.ma@etu.utc.fr>
2023-07-02 04:39:56 +0200

    :art:

--- data.py
+++ data.py
@@ -1,5 +1,14 @@
 from model import *
 
+NB_REFINE = 4
+NB_COMPONENT = 3
+
+FINAL_REFINED = "Coreium Alloy"
+# FINAL_REFINED = "Kriptonite Alloy"
+# FINAL_COMPO = "Surface Scanner"
+FINAL_COMPONENT = "Planet Dust Collector"
+
+
 ORES = {
     "Carbon": Ores("Carbon", 1),
     "Tin": Ores("Tin", 2),
@@ -10,6 +19,21 @@ ORES = {
     "Einherjer": Ores("Einherjer", 75),
     "Dark Matter": Ores("Dark Matter", 160),
     "Kriptonite": Ores("Kriptonite", 340),
+    "Coreium": Ores("Coreium", 730),
+}
+
+refined_values = {
+    "Refined Carbon": Value(k=1.67),
+    "Refined Tin": Value(k=3.45),
+    "Refined Cobalt": Value(k=7.01),
+    "Refined Bismuth": Value(k=14.37),
+    "Refined Cerussite": Value(k=31.74),
+    "Refined Manganese": Value(k=69),
+    "Refined Einherjer": Value(k=138),
+    "Manganese Alloy": Value(k=269.1),
+    "Cobalt Alloy": Value(k=391),
+    "Kriptonite Alloy": Value(k=897),
+    "Coreium Alloy": Value(m=1.87),
 }
 
 refined_ingredients = {
@@ -23,40 +47,12 @@ refined_ingredients = {
     "Manganese Alloy": ([("Refined Manganese", 2), ("Refined Carbon", 10)], Time(s=192)),
     "Cobalt Alloy": ([("Refined Cobalt", 15), ("Refined Carbon", 30)], Time(s=384)),
     "Kriptonite Alloy": ([("Refined Einherjer", 2), ("Kriptonite", 1000)], Time(s=480)),
-}
-
-refined_values = {
-    "Refined Carbon": Value(k=1.67),
-    "Refined Tin": Value(k=3.45),
-    "Refined Cobalt": Value(k=7.01),
-    "Refined Bismuth": Value(k=14.37),
-    "Refined Cerussite": Value(k=31.74),
-    "Refined Manganese": Value(k=69),
-    "Refined Einherjer": Value(k=138),
-    "Manganese Alloy": Value(k=269.1),
-    "Kriptonite Alloy": Value(k=897),
-    "Cobalt Alloy": Value(k=391),
+    "Coreium Alloy": ([("Manganese Alloy", 2), ("Coreium", 1000)], Time(s=576)),
 }
 
 REFINED = {}
 for i in refined_ingredients.keys():
     REFINED[i] = Refined(i, refined_values[i], refined_ingredients[i][0], refined_ingredients[i][1])
-
-component_ingredients = {
-    "Cables": ([("Refined Carbon", 5)], Time(s=48)),
-    "Fuse": ([("Refined Tin", 5)], Time(s=96)),
-    "Heat sensor": ([("Cables", 2), ("Refined Carbon", 10)], Time(s=192)),
-    "Ball bearing": ([("Fuse", 2), ("Refined Cobalt", 5)], Time(s=384)),
-    "Glass": ([("Refined Bismuth", 10)], Time(s=576)),
-    "Circuit": ([("Refined Cerussite", 5), ("Refined Bismuth", 5), ("Cables", 10)], Time(s=960)),
-    "Lense": ([("Glass", 1), ("Refined Manganese", 5)], Time(m=32)),
-    "Laser Optic": ([("Refined Einherjer", 5), ("Refined Tin", 10), ("Lense", 1)], Time(m=48)),
-    "Mini rover": ([("Refined Manganese", 5), ("Circuit", 5), ("Ball bearing", 4)], Time(m=64)),
-    "Solar Panel": ([("Circuit", 5), ("Glass", 10)], Time(m=80)),
-    "Laser blaster": ([("Manganese Alloy", 5), ("Laser Optic", 2), ("Lense", 5)], Time(m=96)),
-    "Advanced Sensors": ([("Cobalt Alloy", 20), ("Heat sensor", 30)], Time(h=2)),
-    "Surface Scanner": ([("Kriptonite Alloy", 5), ("Laser Optic", 2), ("Glass", 5)], Time(h=2, m=24)),
-}
 
 component_values = {
     "Cables": Value(k=11.5),
@@ -72,6 +68,26 @@ component_values = {
     "Laser blaster": Value(m=34.5),
     "Advanced Sensors": Value(m=40.25),
     "Surface Scanner": Value(m=82.22),
+    "Planet Explorer": Value(m=207),
+    "Planet Dust Collector": Value(b=1.10),
+}
+
+component_ingredients = {
+    "Cables": ([("Refined Carbon", 5)], Time(s=48)),
+    "Fuse": ([("Refined Tin", 5)], Time(s=96)),
+    "Heat sensor": ([("Cables", 2), ("Refined Carbon", 10)], Time(s=192)),
+    "Ball bearing": ([("Fuse", 2), ("Refined Cobalt", 5)], Time(s=384)),
+    "Glass": ([("Refined Bismuth", 10)], Time(s=576)),
+    "Circuit": ([("Refined Cerussite", 5), ("Refined Bismuth", 5), ("Cables", 10)], Time(s=960)),
+    "Lense": ([("Glass", 1), ("Refined Manganese", 5)], Time(m=32)),
+    "Laser Optic": ([("Refined Einherjer", 5), ("Refined Tin", 10), ("Lense", 1)], Time(m=48)),
+    "Mini rover": ([("Refined Manganese", 5), ("Circuit", 5), ("Ball bearing", 4)], Time(m=64)),
+    "Solar Panel": ([("Circuit", 5), ("Glass", 10)], Time(m=80)),
+    "Laser blaster": ([("Manganese Alloy", 5), ("Laser Optic", 2), ("Lense", 5)], Time(m=96)),
+    "Advanced Sensors": ([("Cobalt Alloy", 20), ("Heat sensor", 30)], Time(h=2)),
+    "Surface Scanner": ([("Kriptonite Alloy", 5), ("Laser Optic", 2), ("Glass", 5)], Time(h=2, m=24)),
+    "Planet Explorer": ([("Coreium Alloy", 5), ("Mini rover", 5)], Time(h=2, m=48)),
+    "Planet Dust Collector": ([("Laser blaster", 2), ("Surface Scanner", 1), ("Manganese Alloy", 10)], Time(h=3)),
 }
 
 COMPONENTS = {}
--- production.py
+++ production.py
@@ -1,23 +1,23 @@
 from data import *
 
-def listIngredients(compo, ingre_list=INGREDIENTS_LIST):
+def listIngredients(compo):
     ls = {}
     ls[compo.name] = 1
     if isinstance(compo, Ores):
         return ls
     for i in compo.ingredients:
-        tmp = listIngredients(ingre_list[i[0]])
+        tmp = listIngredients(INGREDIENTS_LIST[i[0]])
         for j in tmp:
             if j not in ls:
                 ls[j] = 0
             ls[j] += tmp[j] * i[1]
     return ls
 
-def countIngredients(ingre_list, cond):
+def countIngredients(ingre_list, prod_list):
     time = 0
     gain = 0
-    for i in ingre_list:
-        if cond(i):
+    for i in prod_list:
+        if i in ingre_list:
             print("{}, {} * {}".format(i, INGREDIENTS_LIST[i].time, ingre_list[i]))
             time += INGREDIENTS_LIST[i].time.toInt() * ingre_list[i]
             gain += INGREDIENTS_LIST[i].addedValue.toInt() * ingre_list[i]
@@ -25,13 +25,14 @@ def countIngredients(ingre_list, cond):
 
 def calcGain(prod):
     ls = listIngredients(prod)
-    print("Component:")
-    res_compo = countIngredients(ls, lambda x: isinstance(INGREDIENTS_LIST[x], Component))
     print("Refined:")
-    res_refined = countIngredients(ls, lambda x: isinstance(INGREDIENTS_LIST[x], Refined))
+    res_refined = countIngredients(ls, REFINED)
+    print("Component:")
+    res_compo = countIngredients(ls, COMPONENTS)
     return *res_refined, *res_compo
 
 def showGain(prod, time_refined, gain_refined, time_component, gain_component):
+    print("Avg:")
     print("Refined Avg Gain: {}/h, Total Time: {}".format(Value(gain_refined/time_refined*3600), Time(time_refined)))
     if time_component != 0:
         print("Component Avg Gain: {}/h, Total Time: {}".format(Value(gain_component/time_component*3600), Time(time_component)))
@@ -39,9 +40,8 @@ def showGain(prod, time_refined, gain_refined, time_component, gain_component):
 
 def main():
     prods = [
-        "Kriptonite Alloy",
-        # "Laser blaster",
-        "Surface Scanner",
+        FINAL_COMPONENT,
+        FINAL_REFINED,
     ]
     # prods = {**refined, **components}
     res = {}
@@ -50,11 +50,9 @@ def main():
         res[i] = calcGain(INGREDIENTS_LIST[i])
         showGain(i, *res[i])
         print()
-    final_compo = "Surface Scanner"
-    final_refined = "Kriptonite Alloy"
-    c = res[final_compo]
-    r = res[final_refined]
-    final_gainPerSecond = c[3]/c[2] * 2 + (c[1]/c[2] + r[1]/(c[2]-c[0])) * 2 + r[1]/r[0] * 1
+    c = res[FINAL_COMPONENT]
+    r = res[FINAL_REFINED]
+    final_gainPerSecond = c[3]/c[2] * NB_COMPONENT + (c[1]/c[2] + r[1]/(c[2]-c[0])) * NB_COMPONENT + r[1]/r[0] * (NB_REFINE - NB_COMPONENT)
     print("Final Avg Gain: {}/h".format(Value(final_gainPerSecond*3600)))
     print("Final Time: {}".format(Time(Value(b=10).toInt()/final_gainPerSecond)))
 
